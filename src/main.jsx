34fc5e8f7fbe3323cbcfa8e8d0664b3576381c4f
gojo_Satoru <vydeeshp@gmail.com>
2026-01-14 13:59:17 +0530

    added nrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 // import "./styles/style.css";
+import {Routes, Route,BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <BrowserRouter basename="/vydeeshp.github.io/">
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
